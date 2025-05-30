1c1b783de51b8d6ace2181a3d6093c6d169e42c5
Abdul Salam <abdulsalamahmed9030@gmail.com>
2025-05-30 18:32:08 +0530

    preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,11 @@ import TeamSection from './components/TeamSection'
 import StatisticsSection from './components/Statistics'
 import MapSection from './components/MapSection'
 import Copyright from './components/Copyright'
+import Preloader from './components/Preloader'
 const page = () => {
   return (
     <>
+    <Preloader />
     <Header />
     <HeroSection />
     <AboutUs />
